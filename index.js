5131913e58d7aabdde0fcb6c913fe7382e226e3c
srinathreddy45 <srinathreddy45.sr@gmail.com>
2017-09-25 22:47:23 -0700

    getting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-//Getting response updated
+//Getting response
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Docker World!");
 
